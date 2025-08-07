5e0ffc20ffa479e69ff34f87f800487c28a17478
xinmeimacapagal2007-sketch <xinmeimacapagl2007@gmail.com>
2025-08-07 21:07:03 +0800

    second-commit

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8"> <!-- emoji -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--device:determined by the size of the sceen -->
-    <title>Document</title> <!-- Title of the document -->
-
-    <meta name=title content="Document"> <!-- Title for search engines -->
-    <meta name="description" content="This is a sample document."> <!-- Description for search engines -->
-    <meta name="keywords" content="HTML, CSS, JavaScript"> <!-- Keywords for search engines -->
-    <meta name="author" content="Your Name"> <!--Author of the document -->
-
-   <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous"> <link rel="stylesheet" href="styles.css">-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"> <!--Link to external CSS file -->
-</head>
-<body>
+<?php require_once "partials/head.php" ?>
     <?php
     include_once 'Classes/Order.php';
     include_once 'data/order.php';
@@ -183,7 +168,5 @@
     </div>
 -->
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php require_once "partials/foot.php" ?>
+   
---
+++ partials/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> <!-- emoji -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--device:determined by the size of the sceen -->
+    <title>Document</title> <!-- Title of the document -->
+
+    <meta name=title content="Document"> <!-- Title for search engines -->
+    <meta name="description" content="This is a sample document."> <!-- Description for search engines -->
+    <meta name="keywords" content="HTML, CSS, JavaScript"> <!-- Keywords for search engines -->
+    <meta name="author" content="Your Name"> <!--Author of the document -->
+
+   <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous"> <link rel="stylesheet" href="styles.css">-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"> <!--Link to external CSS file -->
+</head>
+<body>
+
